c8a4c37db0e83a0c5e75870664427f17e9814547
Cerebral <55196334+cerebrallolzy@users.noreply.github.com>
2022-12-14 06:20:24 -0600

    Typo

--- src/js/main.js
+++ src/js/main.js
@@ -174,7 +174,7 @@ global.updateLoop = null;
 function startUpdateLoop()
 {
     let downloadInterval = 0;
-    if (global.updateLoop != null)
+    if (global.updateLoop == null)
     {
         global.updateLoop = setInterval(() => {
             downloadInterval++;
